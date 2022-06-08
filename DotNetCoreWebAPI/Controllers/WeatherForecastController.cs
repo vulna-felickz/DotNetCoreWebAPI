f97f065a36e74d802e436972b396ed3b1c58f3a4
felickz-admin <felickz-admin@felickz-vm-vs20>
2022-06-08 15:45:19 +0000

    Add project files.

---
+++ DotNetCoreWebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        /// <summary>
+        /// Gets by the ZIP (USA)
+        /// </summary>
+        /// <param name="weatherForeCastInputModel"></param>
+        /// <returns></returns>
+        [HttpGet(Name = "GetWeatherForecastByZip")]
+        public IEnumerable<WeatherForecast> GetByZip([FromQuery] WeatherForeCastInputModel weatherForeCastInputModel)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ DotNetCoreWebAPI/Models/WeatherForeCastInputModel.cs
@@ -0,0 +1,21 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCoreWebAPI.Controllers
+{
+    public class WeatherForeCastInputModel
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// The ZipCode (USA)
+        /// </summary>
+        [Required]
+        [SwaggerSchema(Description = "ZipCode (USA)")]
+        public string ZipCode { get; set; } = null!;
+
+        [DefaultValue(false)]
+        public bool IsComplete { get; set; }
+    }
+}
